6185ff6b6c1b81bf362cf881cb653a415e052887
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-09-27 07:30:03 +0100

    dj_database_url

--- atelier_noir_clothing/settings.py
+++ atelier_noir_clothing/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import environ
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,16 +129,21 @@ MEDIA_ROOT = BASE_DIR / "media"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB'),
-        'USER': env('POSTGRES_USER'),
-        'PASSWORD': env('POSTGRES_PASSWORD'),
-        'HOST': 'db',   # service name in docker-compose.yml
-        'PORT': 5432,
-    }
+    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': env('POSTGRES_DB'),
+#         'USER': env('POSTGRES_USER'),
+#         'PASSWORD': env('POSTGRES_PASSWORD'),
+#         'HOST': 'db',   # service name in docker-compose.yml
+#         'PORT': 5432,
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
