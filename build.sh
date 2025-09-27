c246d5b8bb8796529a1482cc1e55aa40b8318b50
Chinonso Wisdom <nonsowisdom62@gmail.com>
2025-09-27 09:55:13 +0100

    gunicorn

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
+python manage.py makemigrations --noinput
 python manage.py migrate
 
 # Create superuser if not exists
